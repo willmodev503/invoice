cee44a948b12024e6d73cce8093302dcc7b55319
Willian Morales <you@example.comwilliamazin@gmail.com>
2026-02-11 08:33:18 -0600

    se agrego action, para  crear invoice y componentes par a crear invoice

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -20,13 +20,13 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   
   
   
-  // const allPages = generatePagination(currentPage, totalPages);
+   const allPages = generatePagination(currentPage, totalPages);
 
   return (
     <>
       {/*  NOTE: Uncomment this code in Chapter 10 */}
 
-      {/* <div className="inline-flex">
+       <div className="inline-flex">
         <PaginationArrow
           direction="left"
           href={createPageURL(currentPage - 1)}
@@ -59,7 +59,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
           href={createPageURL(currentPage + 1)}
           isDisabled={currentPage >= totalPages}
         />
-      </div> */}
+      </div> 
     </>
   );
 }
