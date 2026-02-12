21f8279da699724cab4fc9de05156fc92b0b44ce
Willian Morales <you@example.comwilliamazin@gmail.com>
2026-02-11 21:27:58 -0600

    se creoron action para  editar y borrar, ademas de  manejo de  errores

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,6 +9,9 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
+import { updateInvoice } from '@/app/lib/actions';
+
+
 
 export default function EditInvoiceForm({
   invoice,
@@ -17,8 +20,8 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-  return (
-    <form>
+    const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+ return <form action={updateInvoiceWithId}>{/* ... */}
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -119,5 +122,4 @@ export default function EditInvoiceForm({
         <Button type="submit">Edit Invoice</Button>
       </div>
     </form>
-  );
 }
