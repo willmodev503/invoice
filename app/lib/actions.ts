516e5eb4bf5692a4f8d9df1b1f5c347c0de41803
Willian Morales <you@example.comwilliamazin@gmail.com>
2026-02-16 10:57:27 -0600

    se arreglo error en amount para  server action createinvoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -45,7 +45,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       message: 'Missing Fields. Failed to Create Invoice.',
     };
   }
-
+ const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
       const date = new Date().toISOString().split('T')[0];
 
