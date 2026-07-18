1209752ecd242e010edab5e78ad96879f0335cd0
Willian Morales <you@example.comwilliamazin@gmail.com>
2026-07-17 18:07:19 -0600

    se agrego la opcion new evidence a la pagina de  evidencia

--- app/dashboard/evidences/page.tsx
+++ app/dashboard/evidences/page.tsx
@@ -12,6 +12,19 @@ export default async function Page() {
 
   return (
   <div className="grid gap-4 mt-6">
+ <h1>Evidencias</h1>
+
+        <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+              {/* <Search placeholder="Search invoices..." /> */}
+              <Link
+                  href="/dashboard/evidences/new"
+                  className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                >
+                  <span className="hidden md:block"> nueva evidencia</span>{' '}
+                  <p className="h-5 md:ml-4" />
+                </Link>
+            </div>
+
   {evidences.map((e) => (
     <div
       key={e.id}
